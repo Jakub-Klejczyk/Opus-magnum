986a911242c41caccb970b5fca1661fd38d4f02f
Jakub <jakub-klejczyk@o2.pl>
2022-10-20 21:08:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
